9cea281e36bf0fc228b23b2313e8a4a541dcb3eb
LachlanStephan <ljstephan116@gmail.com>
2026-04-12 12:05:17 +1000

    scaffold app work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::env::home_dir;
+use core::panic;
+use std::{env::home_dir, fs::{self, File}, path::Path};
 
 #[derive(Debug)]
 struct ConfigPaths {
@@ -14,8 +15,8 @@ struct Application {
 
 fn main() {
     let root = String::from(get_home_dir());
-    let note = root.clone() + "/" + "nk-note.md";
-    let config = root.clone() + "/" + "nk-config.txt";
+    let note = root.clone() + "/" + "note.md";
+    let config = root.clone() + "/" + "config.json";
 
     let app = Application {
         config_paths: ConfigPaths {
@@ -25,7 +26,10 @@ fn main() {
         }
     };
 
-    println!("{:?}", app);
+    if !file_exists(&app.config_paths.root_dir_path) {
+        println!("we need to create the stuff");
+        scaffold_app(&app);
+    }
 }
 
 fn get_home_dir() -> String {
@@ -36,4 +40,36 @@ fn get_home_dir() -> String {
     };
 
     return path + "/" + "note-keeper"; // append app root dir
+}
+
+fn file_exists(file_path: &String) -> bool {
+    return Path::new(file_path).exists();
+}
+
+fn scaffold_app(app: &Application) {
+    create_dir(&app.config_paths.root_dir_path);
+    create_file(&app.config_paths.config_file_path);
+    create_file(&app.config_paths.note_file_path);
+    write_file(&app.config_paths.note_file_path, "# Note");
+}
+
+fn create_dir(dir_path: &String) {
+    let result = fs::create_dir(dir_path);
+    if !result.is_ok() {
+        panic!("could not create dir: {}", dir_path);
+    }
+}
+
+fn create_file(file_path: &String) {
+    let result = File::create(file_path);
+    if !result.is_ok() {
+        panic!("could create file: {}", file_path);
+    }
+}
+
+fn write_file(file_path: &String, contents: &str) {
+    let result = fs::write(file_path, contents);
+    if !result.is_ok() {
+        panic!("could not write to file: {}", file_path);
+    }
 }
